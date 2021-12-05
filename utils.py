01a3c0de461e58ca28dbd5e08627970237ea360a
Claudiu Hirsina <claudiu.hirsina@gmail.com>
2021-12-05 17:11:46 +0200

    -improve utils

--- utils.py
+++ utils.py
@@ -2,13 +2,11 @@ from config import mysql
 import pymysql
 
 
-def insert_db(table_name, json):
+def execute_query(sqlQuery):
     conn = cursor = False
     try:
         conn = mysql.connect()
         cursor = conn.cursor()
-        table_columns, values = list(json.keys()), list(json.values())
-        sqlQuery = 'INSERT INTO %s(%s) VALUES("%s")' % (table_name, ','.join(table_columns), '","'.join([str(value) for value in values]))
         cursor.execute(sqlQuery)
         conn.commit()
         return False
@@ -19,6 +17,23 @@ def insert_db(table_name, json):
             cursor.close()
         if conn:
             conn.close()
+
+
+def insert_db(table_name, json):
+    table_columns, values = list(json.keys()), list(json.values())
+    sqlQuery = 'INSERT INTO %s(%s) VALUES("%s")' % (table_name, ','.join(table_columns), '","'.join([str(value) for value in values]))
+    return execute_query(sqlQuery)
+
+
+def update_db(table_name, json, id):
+    update_columns = ",".join([el + " = '" + json[el] + "'" for el in json.keys()])
+    sqlQuery = "UPDATE %s SET %s WHERE id=%s" % (table_name, update_columns, id)
+    return execute_query(sqlQuery)
+
+
+def delete_db(table_name, id):
+    sqlQuery = "DELETE FROM %s WHERE id =%s" % (table_name, id)
+    return execute_query(sqlQuery)
 
 
 def get_db(table_name, columns, id, join='', where=''):
@@ -32,43 +47,6 @@ def get_db(table_name, columns, id, join='', where=''):
         cursor.execute(query)
         rows = id and cursor.fetchone() or cursor.fetchall()
         return rows
-    except Exception as e:
-        return str(e)
-    finally:
-        if cursor:
-            cursor.close()
-        if conn:
-            conn.close()
-
-
-def update_db(table_name, json, id):
-    conn = cursor = False
-    try:
-        conn = mysql.connect()
-        cursor = conn.cursor()
-        update_columns = ",".join([el + " = '" + json[el] + "'" for el in json.keys()])
-        sqlQuery = "UPDATE %s SET %s WHERE id=%s" % (table_name, update_columns, id)
-        cursor.execute(sqlQuery)
-        conn.commit()
-        return False
-    except Exception as e:
-        return str(e)
-    finally:
-        if cursor:
-            cursor.close()
-        if conn:
-            conn.close()
-
-
-def delete_db(table_name, id):
-    conn = cursor = False
-    try:
-        conn = mysql.connect()
-        cursor = conn.cursor()
-        sqlQuery = "DELETE FROM %s WHERE id =%s" % (table_name, id)
-        cursor.execute(sqlQuery)
-        conn.commit()
-        return False
     except Exception as e:
         return str(e)
     finally:
